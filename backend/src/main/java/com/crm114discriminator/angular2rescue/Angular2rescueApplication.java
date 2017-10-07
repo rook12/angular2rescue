66ace92c618a5724ddd90c5e5aaf815fcb986108
Steven Murphy <steve231@gmail.com>
2017-10-07 17:59:18 +0100

    Initial commit

---
+++ backend/src/main/java/com/crm114discriminator/angular2rescue/Angular2rescueApplication.java
@@ -0,0 +1,12 @@
+package com.crm114discriminator.angular2rescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular2rescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular2rescueApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/crm114discriminator/angular2rescue/Angular2rescueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crm114discriminator.angular2rescue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Angular2rescueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
