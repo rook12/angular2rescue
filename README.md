6c120c4d1deb45ddd0301f48584c8905017f6f84
Steven Murphy <steve231@gmail.com>
2017-10-28 16:26:02 +0100

    Add navbar and imported ng-bootstrap, updated readme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 * Based on https://blog.jdriven.com/2016/12/angular2-spring-boot-getting-started/
 * Angular architecture - https://angular.io/guide/architecture#templates
+* Routing - https://angular.io/guide/router
 
 # Starting up for DEV
 
@@ -14,15 +15,30 @@
 * Routing (still to look at properly) - https://angular.io/guide/router
 * Play with implementing some sort of message handling in back end.
 * Securing pages / Login system
-* Spring boot backend API for pulling data from a DB
+* Spring boot backend API for pulling data from a DB (or simulate it https://angular.io/tutorial/toh-pt6)
 * Try implementing a sub module of rescue unit
-
+* PrimeNG for UI components
 
 ## To create a new component
 
 * Create a folder where you want the component
 * Note this doesn't work in IntelliJ
 * Navigate to component directory, then ng generate component componentname --skip-import
+
+## Importing packages to npm
+
+https://medium.com/codingthesmartway-com-blog/using-bootstrap-with-angular-c83c3cee3f4a
+
+npm install bootstrap@3 jquery --save
+
+Installs bootstrap v3 and jquery, and puts it into package.json
+
+Alternately, use ng-bootstrap designed specifically for Angular
+
+npm install --save @ng-bootstrap/ng-bootstrap
+npm install bootstrap@4.0.0-alpha.6
+
+
 
 ## Debugging
 
--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -8,26 +8,32 @@ import {AppRoutingModule} from "./app-routing.module";
 
 import {Router} from "@angular/router";
 import {UnitrootComponent} from "./unit/unitroot/unitroot.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbCollapseModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   //the view classes that belong to this module. Angular has three kinds of view classes: components, directives, and pipes.
   declarations: [
     RescueRootAppComponent,
     HellooneComponent,
-    UnitrootComponent
+    UnitrootComponent,
+    NavbarComponent
   ],
 
   //other modules whose exported classes are needed by component templates declared in this module.
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    NgbCollapseModule.forRoot()
   ],
 
   //creators of services that this module contributes to the global collection of services; they become accessible in all parts of the app.
   //Things in here can then be injected into classes that require them as dependencies (can also do in the components)
   //Gets injected through component constructor, eg constructor(private service: HeroService) { }
   providers: [],
-
 
   //the main application view, called the root component, that hosts all other app views. Only the root module should set this bootstrap property.
   bootstrap: [RescueRootAppComponent]
